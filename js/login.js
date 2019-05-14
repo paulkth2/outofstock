2acdb695ffd20abdef543349d30bed0f264aa70e
Taehyung Kim <paulkth2@kaist.ac.kr>
2019-05-15 01:33:17 +0900

    login success

--- js/login.js
+++ js/login.js
@@ -48,7 +48,19 @@ function register(){
   });
 }
 
+function login(){
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("pw").value;
 
+  firebase.auth().signInWithEmailAndPassword(email, password).then(function(User){
+    window.location.href = './makecard.html';
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
 /*
 * working UI part of code
 *
