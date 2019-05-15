96c2541362265234d2a3901d8ccd61297519c1f5
EOMMINHO <djaalsgh159@kaist.ac.kr>
2019-05-15 14:14:04 +0900

    Add files via upload

--- js/gather_img.js
+++ js/gather_img.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 
   $(document).on("click", ".btn-success", function(){
     var img_src = $(this).closest(".card").find("img").attr("src")
+    var card_title = $(this).closest(".card").find(".card-title").html()
     localStorage.setItem("sending_card", img_src)
+    localStorage.setItem("card_title", card_title)
     //console.log(img_src)
     //console.log("here")
   })
