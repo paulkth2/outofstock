91c1ca880821ba91ef780c22ded1c1c31b8260b8
EOMMINHO <djaalsgh159@kaist.ac.kr>
2019-05-24 22:15:30 +0900

    Customer part
    customer part has changed:
    1) The last date
    2) Input to the favorite food
    3) Recommender system.

--- js/Send.js
+++ js/Send.js
@@ -1,3 +1,24 @@
+//This function calculates the date between current date and the last date
+function betweenDate(last_date){
+  // format of last_date : e.g.)2019-1-23
+  var dateList = last_date.split("-");
+  var month = parseInt(dateList[0]);
+  var date = parseInt(dateList[1]);
+  var year = parseInt(dateList[2]);
+  var date1 = new Date(year, month - 1, date);
+
+  var current_date = new Date();
+  var diff = Math.abs(current_date.getTime() - date1.getTime());
+  var diff_days = Math.ceil(diff / (1000 * 3600 * 24))
+
+  return diff_days;
+}
+
+//delete a customer
+function deleteFromDatebase(uid, name){
+  firebase.database().ref(uid).child("customer").child(name).set({});
+}
+
 function writeToDatabase(uid, name, address, rec_menu){
   var newkey = firebase.database().ref(uid).child("customer").child(name).set({
     address: address,
@@ -12,7 +33,9 @@ function readFromDatabase(uid,name){
   })
 }
 
+/*
 function readMenu(uid){
+
   var foodList = []
   firebase.database().ref(uid+"/menus/").once('value',function(snapshot){
     var myValue = snapshot.val(); //{address:"xxx@bbb.ccc", menu:"xxx", status:"xxx"}
@@ -21,50 +44,40 @@ function readMenu(uid){
       foodList.push(myValue[key]["name"])
     }
     console.log(foodList)
-  }).then(readFromDatabaseAll(uid,foodList))
-}
 
-/*
-<tr class="table-danger">
-  <td>
-    <input id="rec_0" class="check" type="checkbox">
-  </td>
-  <td>
-    (FOOD)
-  </td>
-  <td class="address">
-    (628)xxx-xxxx
-  </td>
-  <td>
-    VVIP
-  </td>
-</tr>
-*/
+    console.log("here")
+  }).then(readFromDatabaseAll(uid))
+}*/
 
-function readFromDatabaseAll(uid, send_food){
+function readFromDatabaseAll(uid){
   firebase.database().ref(uid+"/customer/").once('value',function(snapshot){
     var myValue = snapshot.val(); //{address:"xxx@bbb.ccc", menu:"xxx"}
     //var i = Object.keys(myValue).length - 1;
     var key = Object.keys(myValue);
+    if(key == null){
+      return;
+    }
     key.sort((a, b) => a.localeCompare(b, 'fr', {ignorePunctuation: true})).reverse()
     var i = key.length - 1;
     localStorage.setItem("length2",key.length);
+    var length1 = 0;
     for(var k = 0; k < key.length; k++){
       //console.log(key)
       var dict = myValue[key[k]]
       var status;
       var color;
-      //var menu = ["연어회덮밥","참치회덮밥","초밥 셋","컷틝","정식","우동","소바"]; // have to implement
-      var menu = send_food
-      var menu_sel = dict["menu"].split(",");
-      var menu_final = [];
+      //var menu = send_food
+      //var menu_sel = dict["menu"].split(",");
+      //var menu_final = [];
+      /*
       for(var j = 0; j < menu_sel.length; j++){
         menu_final.push(menu[menu_sel[j]]);
       }
+      */
 
       if(dict["status"] == 2){
-        status = "NO"
-        color = "NO"
+        status = "NEW"
+        color = "NEW"
       }else if(dict["status"] == 1){
         status = "VIP"
         color = "table-warning"
@@ -73,19 +86,41 @@ function readFromDatabaseAll(uid, send_food){
         color = "table-danger"
       }
 
+      var address = dict["address"];
+      var last_date = dict["last_date"];
+      var menu = dict["menu"];
+      var between_date = betweenDate(last_date);
+
+      var thres_date = parseInt($("#cur_card").find("strong").html());
+
+      var row_rec = document.getElementById("rec_customers").insertRow(1);
+      if (between_date >= thres_date){
+        row_rec.innerHTML = "<tr><td><input id='rec_" + i + "'class='check' type='checkbox'></td><td class='name'>"+key[k]+"</td><td>"+menu+"</td><td class='address' id=" + address +">" +between_date+" day </td><td>"+status+"</td></tr>"
+        $("#rec_"+i).closest("tr").prop("class",color)
+        length1 = length1 + 1;
+      }
+
       var row = document.getElementById("reg_customers").insertRow(1);
-      row.innerHTML = "<tr><td><input id='reg_" + i + "'class='check' type='checkbox'></td><td>"+key[k]+"</td><td>"+menu_final+"</td><td class='address'>"+dict["address"]+"</td><td>"+status+"</td></tr>"
+      if (between_date == 1){
+        row.innerHTML = "<tr><td><input id='reg_" + i + "'class='check' type='checkbox'></td><td class='name'>"+key[k]+"</td><td>"+menu+"</td><td class='address' id=" + address +">" + between_date + " day </td><td>"+status+"</td></tr>"
+      }else{
+        row.innerHTML = "<tr><td><input id='reg_" + i + "'class='check' type='checkbox'></td><td class='name'>"+key[k]+"</td><td>"+menu+"</td><td class='address' id=" + address +">"+between_date+" days </td><td>"+status+"</td></tr>"
+      }
       $("#reg_"+i).closest("tr").prop("class",color)
       i = i - 1
     }
+    localStorage.setItem("length1",length1);
   })
 }
+
+
+
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
      var myUID = firebase.auth().currentUser.uid;
-     readMenu(myUID)
+     readFromDatabaseAll(myUID)
      //readFromDatabaseAll(myUID) // write a table
   } else {
     // No user is signed in.
@@ -93,56 +128,130 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 
-
-
-
 $(document).ready(function(){
-
-  $("#cur_card").html(localStorage.getItem("card_title"))
-  console.log("ready_send")
   var sending_card = localStorage.getItem("sending_card")
 
-  $(".checkall").on("click",function(){
+  $("#cur_card").find(".dropdown-item").on("click",function(){
+    var date = $(this).html();
+    $("#cur_card").find("strong").html(date);
+
+    $("#rec_customers").find("thead").html("<tr><th><input class='checkall' type='checkbox'></th><th>Name</th><th>Favorite Food</th><th>Last date</th><th>Status</th></tr>")
+    $("#reg_customers").find("thead").html("<tr><th><input class='checkall' type='checkbox'></th><th>Name</th><th>Favorite Food</th><th>Last date</th><th>Status</th></tr>")
+    readFromDatabaseAll(firebase.auth().currentUser.uid)
+  })
+
+
+
+  $(document).on("click",".checkall", function(){
     if($(this).prop("checked")==false){
+      //$(this).closest("table").find("input").prop("checked",false);
+      var hindex = $(this).closest("table").prop("id").split("_")[0];
+      if(hindex == "reg"){
+        $("#rec_customers").find(".checkall").prop("checked",false);
+        var length = localStorage.getItem("length2");
+        for(var i = 0; i < length; i++){
+          $("#rec_"+i).prop("checked", false);
+        }
+      }else{
+        $("#reg_customers").find(".checkall").prop("checked",false);
+        var length = localStorage.getItem("length2");
+        var index = [];
+        for(var i = 0; i < length; i++){
+          if($("#rec_"+i).prop("checked") == true) index.push(i)
+        }
+        for(var i = 0; i < index.length; i++){
+          $("#reg_"+index[i]).prop("checked",false)
+        }
+      }
       $(this).closest("table").find("input").prop("checked",false);
     }else{
       $(this).closest("table").find("input").prop("checked",true);
+      var hindex = $(this).closest("table").prop("id").split("_")[0];
+      if(hindex == "reg"){
+        $("#rec_customers").find(".checkall").prop("checked",true);
+        var length = localStorage.getItem("length2");
+        for(var i = 0; i < length; i++){
+          $("#rec_"+i).prop("checked", true);
+        }
+      }else{
+        var length = localStorage.getItem("length2");
+        var index = [];
+        for(var i = 0; i < length; i++){
+          if($("#rec_"+i).prop("checked") == true) index.push(i)
+        }
+        for(var i = 0; i < index.length; i++){
+          $("#reg_"+index[i]).prop("checked",true)
+        }
+      }
     }
   })
 
-  $(".check").on("click",function(){
+  $(document).on("click",".check",function(){
     if($(this).prop("checked")==true){
       //can implement to check "checkall" button
+      var index = $(this).prop("id").split("_")[1];
+      $("#reg_"+index).prop("checked",true);
+      $("#rec_"+index).prop("checked",true);
     }else{
       $(this).closest("table").find(".checkall").prop("checked",false);
+      var index = $(this).prop("id").split("_")[1];
+      $("#reg_"+index).prop("checked",false);
+      $("#rec_"+index).prop("checked",false);
+    }
+  })
+
+  $(".delete").on("click",function(){
+    var length2 = parseInt(localStorage.getItem("length2"));
+    for(var i = 0; i < length2; i++){
+      if($("#reg_"+i).prop("checked") == true){
+        var name = $("#reg_"+i).closest("tr").find(".name").html();
+        //delete from database;
+        deleteFromDatebase(firebase.auth().currentUser.uid, name);
+        $("#reg_"+i).closest("tr").html("");
+      }
+      if($("#rec_"+i).prop("checked") == true){
+        var name = $("#rec_"+i).closest("tr").find(".name").html();
+        //delete from database;
+        deleteFromDatebase(firebase.auth().currentUser.uid, name);
+        $("#rec_"+i).closest("tr").html("");
+      }
+    }
+    for(var i = 0; i < length2; i++){
+      $("#reg_"+i).prop("checked",false)
+      $("#reg_"+i).closest("table").find(".checkall").prop("checked",false);
+      $("#rec_"+i).prop("checked",false)
+      $("#rec_"+i).closest("table").find(".checkall").prop("checked",false);
     }
   })
 
   $(".btn-send").on("click",function(){
-    var length1 = 3;
+    var length1 = parseInt(localStorage.getItem("length1"));
     var length2 = parseInt(localStorage.getItem("length2"));
     var address = [];
-    for(var i = 0; i < length1; i++){
+    for(var i = 0; i < length2; i++){
       if($("#rec_"+i).prop("checked") == true){
-        var myAdd = $("#rec_"+i).closest("tr").find(".address").html();
+        var myAdd = $("#rec_"+i).closest("tr").find(".address").prop("id");
         address.push(myAdd.trim());
       }
     }
     for(var i = 0; i < length2; i++){
       if($("#reg_"+i).prop("checked") == true){
-        var myAdd = $("#reg_"+i).closest("tr").find(".address").html();
+        var myAdd = $("#reg_"+i).closest("tr").find(".address").prop("id");
         address.push(myAdd.trim());
       }
     }
+    var tmp_set = new Set(address);
+    address = Array.from(tmp_set);
+
     localStorage.setItem("customerList",address)
     if(address.length > 0){
       // have to implement how to send it.
-      var subject = "이 메뉴를 추천드려요:" + localStorage.getItem("card_title");
+      var subject = "이 메뉴를 추천드려요: " + localStorage.getItem("card_title");
       var body = ""+localStorage.getItem("sending_card");
       var link = 'mailto:'+address+'?subject=' +subject+ "&body=" + body;
       window.location.href = link;
       //alert("보내기 완료")
-      for(var i = 0; i < length1; i++){
+      for(var i = 0; i < length2; i++){
         $("#rec_"+i).prop("checked",false)
         $("#rec_"+i).closest("table").find(".checkall").prop("checked",false);
       }
--- js/customeradd.js
+++ js/customeradd.js
@@ -11,11 +11,12 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 */
-function writeToDatabase(uid, name, address, rec_menu, status){
+function writeToDatabase(uid, name, address, rec_menu, status, last_date){
   var newkey = firebase.database().ref(uid).child("customer").child(name).set({
     address: address,
     menu: rec_menu,
-    status: status
+    status: status,
+    last_date: last_date
   });
 }
 
@@ -58,54 +59,44 @@ function readFromDatabase(uid,name){
 function readFromDatabaseMenu(uid, new_val){
   firebase.database().ref(uid+"/menus").once('value',function(snapshot){
     var myValue = snapshot.val(); //{address:"xxx@bbb.ccc", menu:"xxx"}
+    /*
     var keys = Object.keys(myValue);
     var tmp_list = [];
     for(var i = 0; i < keys.length; i++){
       var name = myValue[keys[i]]["name"]
       tmp_list.push(name);
-      $("#menus").append("<input id='ch"+ i +"' type='checkbox'>"+name+" <br>")
     }
-    /*
-    if(new_val){
-      tmp_list.push(new_val);
-      $("#menus").append("<input id='ch"+ keys.length +"' type='checkbox'>"+new_val+" <button type='button' class='btn btn-outline-danger btn-sm'>삭제</button><br>")
-      console.log("newval here")
-    }
-    */
     localStorage.setItem("cur_value", tmp_list)
+    */
   }).then(function(){
     $(document).on("click", ".btn-success", function(){
       var name = $("#exampleInputEmail1").val();
       var address = $("#exampleInputPassword1").val();
       var status = 2; // 0:VVIP 1:VIP 2:NEW
       var rec_menu = [];
-      var menu_str;
+      var menu_str = $(".favorite_food").val();
 
-      var cur_value = localStorage.getItem("cur_value").split(",");
-      var length = cur_value.length;
-      console.log(cur_value)
-      for(var i = 0; i < length; i++){
-        if($("#ch" + i).prop("checked") == true){
-          rec_menu.push(i);
-        }
-      }
+      //var cur_value = localStorage.getItem("cur_value").split(",");
+      //var length = cur_value.length;
       for(var i = 0; i < 3; i++){
         if($("#stat" + i).prop("checked") == true){
           status = i;
         }
       }
-      console.log(rec_menu)
-      menu_str = rec_menu.join();
-      writeToDatabase(uid, name, address, menu_str, status)
+
+      var today = new Date();
+      var year = today.getFullYear();
+      var month = today.getMonth() + 1;
+      var date = today.getDate();
+      var ymd = String(month)+"-"+String(date)+"-"+String(year);
+      writeToDatabase(uid, name, address, menu_str, status, ymd)
       alert("등록완료")
       $("#exampleInputEmail1").val("");
       $("#exampleInputPassword1").val("");
+      $(".favorite_food").val("");
       $("#stat0").prop("checked",false);
       $("#stat1").prop("checked",false);
       $("#stat2").prop("checked",true);
-      for(var i = 0; i < length; i++){
-        $("#ch" + i + " ").prop("checked",false);
-      }
     })
   })
 }
@@ -116,91 +107,11 @@ firebase.auth().onAuthStateChanged(function(user) {
      var myUID = firebase.auth().currentUser.uid;
      readFromDatabaseMenu(myUID);
      $(document).ready(function(){
-
-       /*
-       var rec_menu = [];
-       var cur_value = localStorage.getItem("cur_value").split(",");
-       var length = cur_value.length;
-
-       for(var i = 0; i < length; i++){
-         $(document).on("change","#ch"+i+" ", function(){
-           if($(this).prop("checked") == true){
-             console.log("here")
-             rec_menu.push(i);
-           }
-         })
-       }
-       localStorage.setItem("rec_menu",rec_menu)
-       */
-
-       /*
-       $(document).on("click", ".btn-success", function(){
-         var name = $("#exampleInputEmail1").val();
-         var address = $("#exampleInputPassword1").val();
-         var status = 2; // 0:VVIP 1:VIP 2:NEW
-         //var rec_menu = [];
-         var menu_str;
-
-         var cur_value = localStorage.getItem("cur_value").split(",");
-         var length = cur_value.length;
-
-         for(var i = 0; i < 3; i++){
-           if($("#stat" + i).prop("checked") == true){
-             status = i;
-           }
-         }
-         menu_str = rec_menu.join();
-         writeToDatabase(myUID, name, address, menu_str, status)
-         alert("등록완료")
-         $("#exampleInputEmail1").val("");
-         $("#exampleInputPassword1").val("");
-         $("#stat0").prop("checked",false);
-         $("#stat1").prop("checked",false);
-         $("#stat2").prop("checked",true);
-         for(var i = 0; i < length; i++){
-           $("#ch" + i).prop("checked",false);
-         }
-       })
-       */
-       /*
-       $(".btn-success").on("click",function(){
-         var name = $("#exampleInputEmail1").val();
-         var address = $("#exampleInputPassword1").val();
-         var status = 2; // 0:VVIP 1:VIP 2:NEW
-         var rec_menu = [];
-         var menu_str;
-
-         var length = cur_value.length;
-         for(var i = 0; i < length; i++){
-           if($("#ch" + i).prop("checked") == true){
-             rec_menu.push(i);
-           }
-         }
-         for(var i = 0; i < 3; i++){
-           if($("#stat" + i).prop("checked") == true){
-             status = i;
-           }
-         }
-         menu_str = rec_menu.join();
-         writeToDatabase(myUID, name, address, menu_str, status)
-         alert("등록완료")
-         $("#exampleInputEmail1").val("");
-         $("#exampleInputPassword1").val("");
-         $("#stat0").prop("checked",false);
-         $("#stat1").prop("checked",false);
-         $("#stat2").prop("checked",true);
-         for(var i = 0; i < length; i++){
-           $("#ch" + i).prop("checked",false);
-         }
-       })
-       */
-
        $("#menu_btn").on("click",function(){
          var menu = $("#menu_hold").val();
          $("#menus").html("");
          writeToDatabase_ver2(myUID, menu);
          $("#menu_hold").val("");
-         //readFromDatabaseMenu(myUID);
        })
      })
 
